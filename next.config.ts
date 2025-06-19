e86a4ba18a3b63a5e7a138a8ea80e8e6482ac05b
chauid <dlfdl28@naver.com>
2025-06-19 18:01:59 +0900

    이미지 설정

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,24 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'kr.object.ncloudstorage.com',
+        port: '',
+        pathname: '/postsmith-bucket/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
