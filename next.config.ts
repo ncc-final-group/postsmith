448fc788c9e806508610ab372d5be8de6d615f50
Junho <zpzlzmz@naver.com>
2025-06-19 21:18:08 +0900

    메인페이지 완성(스와이프 부분은 스와이프만됨)z

--- next.config.ts
+++ next.config.ts
@@ -10,24 +10,22 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-        search: '',
-      },
-      {
-        protocol: 'https',
-        hostname: 'kr.object.ncloudstorage.com',
-        port: '',
-        pathname: '/postsmith-bucket/**',
-        search: '',
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'images.unsplash.com',
+      port: '',
+      pathname: '/**',
+      search: '',
+    },
+    {
+      protocol: 'https',
+      hostname: 'kr.object.ncloudstorage.com',
+      port: '',
+      pathname: '/postsmith-bucket/**',
+      search: '',
+    },
+  ],
 };
 
 export default nextConfig;
